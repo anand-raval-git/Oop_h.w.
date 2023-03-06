45c5b213054062dcd38a5ee9180502c8f6809434
anandraval4 <anandraval.mu@gmai.com>
2023-03-06 22:33:13 +0530

    Write a java program to print address of marwadi university as follows  a)single print statement b)multiple print statement
    Write a java program to print address of marwadi university as follows
    a)single print statement
    b)multiple print statement

---
+++ 02_address.java
@@ -0,0 +1,11 @@
+class address 
+{
+    public static void main(String[] args)
+    {
+        //Singl line
+        System.out.println("Marwadi university");
+        //Double line
+        System.out.println("Marwadi university");
+        System.out.println("Morbi highway rajkot");
+    }   
+}
